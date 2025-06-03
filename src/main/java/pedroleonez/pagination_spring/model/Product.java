5da59d0b52465932ef1b48f4f31bd2bde2e9cc31
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-06-03 15:35:58 -0300

    feat: create and impl Product model

---
+++ src/main/java/pedroleonez/pagination_spring/model/Product.java
@@ -0,0 +1,39 @@
+package pedroleonez.pagination_spring.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private OffsetDateTime createdAt;
+}
