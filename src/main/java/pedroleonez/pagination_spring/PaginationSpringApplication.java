802e10aca1e8a11fc9ca877294130527b3db9e49
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-06-03 15:35:30 -0300

    initial commit

---
+++ src/main/java/pedroleonez/pagination_spring/PaginationSpringApplication.java
@@ -0,0 +1,13 @@
+package pedroleonez.pagination_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pedroleonez/pagination_spring/PaginationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pedroleonez.pagination_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
